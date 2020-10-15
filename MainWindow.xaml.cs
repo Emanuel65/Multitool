6e3aa0dcf349be589cc32d467c5fce8dc59d4009
DESKTOP-HFTT8Q6\Emanuel <slavescuemanuel@gmail.com>
2020-10-16 00:38:56 +0300

    Beta version
    All functionality present.
    Need to replace place holder region data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
 
@@ -9,19 +11,24 @@ namespace Multitool
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Assign the logical direction forward/backward to a single variable
+        LogicalDirection fwd = LogicalDirection.Forward;
+        LogicalDirection bckwd = LogicalDirection.Backward;
+
+        // Set the temp text to be displayed
+        string placeHolderText = "Generic input box";
+
         public MainWindow()
         {
             InitializeComponent();
+            SetTempText();
         }
 
         private void btn_CompareStrings_Click(object sender, RoutedEventArgs e)
         {
-
-
             // Get the start/end point of the content for box A
             var inputStartA = rtxtBx_A.Document.ContentStart;
             var inputEndA = rtxtBx_A.Document.ContentEnd;
-
 
             // Get the start/end point of the content for box B
             var inputStartB = rtxtBx_B.Document.ContentStart;
@@ -37,8 +44,6 @@ namespace Multitool
             var inputA = new TextRange(inputStartA, inputEndA).Text;
             var inputB = new TextRange(inputStartB, inputEndB).Text;
 
-            // Assign the logical direction forward to a single variable
-            var fwd = LogicalDirection.Forward;
 
             // Get the TextPointer to the start of the text content for text box A and B
             var insStartA = inputStartA.GetInsertionPosition(fwd);
@@ -95,15 +100,13 @@ namespace Multitool
                 if (inputA.Length != inputB.Length)
                 {
                     // Inform the user their lengths are different
-                    txtBlk_Result.Text = "Different length!";
-                    txtBlk_Result.Background = Brushes.LightSalmon;
+                    WrongResult("Different length!");
                 }
                 // Otherwise...
                 else
                 {
-                    // Inform the user they just don't match
-                    txtBlk_Result.Text = "No match!";
-                    txtBlk_Result.Background = Brushes.LightSalmon;
+                    // Inform the user the inputs don't match
+                    WrongResult("No match!");
                 }
             }
 
@@ -112,33 +115,169 @@ namespace Multitool
         // Clear the input
         private void btn_Clear_Click(object sender, RoutedEventArgs e)
         {
-            //Clear both boxes of content
-            rtxtBx_A.Document.Blocks.Clear();
-            rtxtBx_B.Document.Blocks.Clear();
+            // Clear both boxes of content
+            ClearBothBoxes();
 
-            //Resets the text of the result box
-            txtBlk_Result.Text = "Result";
-        }
-
-        //Reset the result box when new text is entered in box A
-        private void rtxtBx_A_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
+            // Resets the text of the result box
             ResetResult();
+
+            // Set the placeholder text back
+            SetTempText();
         }
 
-        //Reset the result box when new text is entered in box A
-        private void rtxtBx_B_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+
+        private void btn_Region_Click(object sender, RoutedEventArgs e)
         {
+            // Reset result to start off clean
             ResetResult();
+
+            // Get the start/end point of the content for box A
+            var inputStartA = rtxtBx_A.Document.ContentStart;
+            var inputEndA = rtxtBx_A.Document.ContentEnd;
+
+            // Get the trimmed content for text box A and B
+            var inputA = GetTextInA(true);
+
+            // If there are at least two characters present...
+            if (inputA.Length >= 2)
+            {
+                // Parse the numbers
+                if (int.TryParse(inputA.Substring(0, 2), out int numbers))
+                {
+                    // Create local dictionary to store the values
+                    Dictionary<int, RegionData> _localDict = RegionData.regionDictionary;
+
+                    // If the numbers provided are 
+                    if (RegionData.regionDictionary.ContainsKey(numbers))
+                    {
+                        txtBlk_Result.Text =
+                        $"Region name: {_localDict[numbers].regionName} " +
+                        $"BRP code: {_localDict[numbers].brpCode}";
+                    }
+                }
+                else
+                {
+                    WrongResult("Invalid input!");
+                }
+            }
         }
 
+        // Clears the temporary text on focus
+        private void rtxtBx_A_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            // Get the current contents of the text box
+            var txt = new TextRange(rtxtBx_A.Document.ContentStart, rtxtBx_A.Document.ContentEnd).Text.Trim();
+
+            // Change the text only if the text is the temp text
+            if (txt.Equals(placeHolderText))
+            {
+                rtxtBx_A.Document.Blocks.Clear();
+            }
+        }
+
+        // Adds the temp text back when focus is lost
+        private void rtxtBx_A_LostFocus(object sender, RoutedEventArgs e)
+        {
+            SetTempText();
+        }
+
+        // Output the text with proper case
+        private void btn_ProperCase_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the text from the box
+            var tempText = GetTextInA(true).ToLower();
+
+            // Set-up a formating option
+            TextInfo txtInf = new CultureInfo("en-US", false).TextInfo;
+
+            // Get the proper case into a string
+            tempText = txtInf.ToTitleCase(tempText);
+
+            txtBlk_Result.Text = tempText;
+
+            //rtxtBx_A.Document.Blocks.Add(new Paragraph(new Run(tempText)));
+        }
+
+        #region Helper Methods
+        /// <summary>
+        /// Provides basic clean-up for the result
+        /// </summary>
         private void ResetResult()
         {
+            // If text is not already "Result"...
             if (txtBlk_Result.Text != "Result")
             {
+                // Sets the text to result
                 txtBlk_Result.Text = "Result";
+                // Sets the background to the default white
                 txtBlk_Result.Background = Brushes.White;
             }
         }
+
+        /// <summary>
+        /// The default method to call when there is an issue
+        /// </summary>
+        /// <param name="errorMessage"></param>
+        private void WrongResult(string errorMessage)
+        {
+            txtBlk_Result.Text = errorMessage;
+            txtBlk_Result.Background = Brushes.LightSalmon;
+        }
+
+        /// <summary>
+        /// Sets the default temp text to be displayed
+        /// </summary>
+        private void SetTempText()
+        {
+            string txt = GetTextInA(false);
+
+            // Change the text only if there is no text
+            if (string.IsNullOrEmpty(txt))
+            {
+                // Sets a content container for the actual text
+                FlowDocument doc = new FlowDocument();
+
+                // Writes the text to a paragraph
+                Paragraph p = new Paragraph(new Run(placeHolderText));
+
+                // Sets the styling
+                p.FontStyle = FontStyles.Italic;
+                p.Foreground = Brushes.Gray;
+
+                // Adds the paragraph to the content container
+                doc.Blocks.Add(p);
+
+                // Displays the text
+                rtxtBx_A.Document = doc;
+            }
+        }
+
+        /// <summary>
+        /// Returns the text string of box A
+        /// </summary>
+        /// <param name="isTrimmed"></param>
+        /// <returns></returns>
+        private string GetTextInA(bool isTrimmed)
+        {
+            // Returns the current contents of the text box
+            return
+                (isTrimmed
+                ? new TextRange(rtxtBx_A.Document.ContentStart, rtxtBx_A.Document.ContentEnd).Text.Trim()
+                : new TextRange(rtxtBx_A.Document.ContentStart, rtxtBx_A.Document.ContentEnd).Text);
+
+        }
+
+        /// <summary>
+        /// Clears both text boxes
+        /// </summary>
+        private void ClearBothBoxes()
+        {
+            rtxtBx_A.Document.Blocks.Clear();
+            rtxtBx_B.Document.Blocks.Clear();
+        }
+
+        #endregion
+
     }
 }
---
+++ RegionData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Multitool
+{
+    class RegionData
+    {
+        public int gstCode { get; private set; }
+        public int brpCode { get; private set; }
+        public string regionName { get; private set; }
+
+        private static RegionData region1 = new RegionData() { gstCode = 21, brpCode = 51, regionName = "Region1" };
+        private static RegionData region2 = new RegionData() { gstCode = 22, brpCode = 52, regionName = "Region2" };
+        private static RegionData region3 = new RegionData() { gstCode = 23, brpCode = 53, regionName = "Region3" };
+        private static RegionData region4 = new RegionData() { gstCode = 24, brpCode = 54, regionName = "Region4" };
+        private static RegionData region5 = new RegionData() { gstCode = 25, brpCode = 55, regionName = "Region5" };
+
+        public static Dictionary<int, RegionData> regionDictionary = new Dictionary<int, RegionData>
+        {
+            { region1.gstCode, region1 },
+            { region2.gstCode, region2 },
+            { region3.gstCode, region3 },
+            { region4.gstCode, region4 },
+            { region5.gstCode, region5 }
+        };
+
+    }
+
+}
